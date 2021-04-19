81b36729b18c8868c9488bfa49e64f3185c105dd
Mahmood Heidari <mahmoodh1378@gmail.com>
2021-04-19 17:23:21 +0430

    ✅ scroll added for boxes

---
+++ scripts/scroll.gd
@@ -0,0 +1,34 @@
+extends Control
+
+
+signal play
+signal play_backward
+
+onready var root = get_tree().root.get_child(0)
+onready var player = root.find_node('player')
+onready var margin: MarginContainer = get_node('./scrollPicMargin')
+onready var animation: AnimationPlayer = get_node('./animation')
+onready var title: Label = find_node('title')
+onready var context: Label = find_node('context')
+var backward: bool = false
+
+func _ready():
+	self.visible = false
+	self.connect("play", self, "play_func")
+
+func _process(delta):
+	margin.rect_pivot_offset = margin.rect_size/2
+	if Input.is_action_just_pressed("ui_attack") and backward:
+		play_backward_func()
+
+func play_func(attr: Array) -> void:
+	title.text = attr[0].replace('\\n', '\n')
+	context.text = attr[1].replace('\\n', '\n')
+	margin.rect_scale = Vector2(0, 0)
+	animation.play("scroll_open")
+	self.visible = true
+	backward = true
+
+func play_backward_func() -> void:
+	animation.play_backwards("scroll_open")
+	player.emit_signal('allow_move')
