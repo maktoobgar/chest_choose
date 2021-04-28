8046974cbba4b9ca4b29d97b87095f47fcde7221
Mahmood Heidari <mahmoodh1378@gmail.com>
2021-04-28 15:20:00 +0430

    ✅ ChatController.js file added

--- backend/app/Controllers/Ws/ChatController.js
+++ backend/app/Controllers/Ws/ChatController.js
@@ -4,11 +4,10 @@ class ChatController {
   constructor ({ socket, request }) {
     this.socket = socket
     this.request = request
-    console.log('print')
+    console.log('connected')
   }
   onMessage (message) {
-    this.socket.broadcastToAll('message', message)
-    console.log('print')
+    console.log(message)
   }
 }
 
