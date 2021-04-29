0c2f245d644ac19013ea82d6512bb5a1b63833e0
Mahmood Heidari <mahmoodh1378@gmail.com>
2021-04-29 18:50:42 +0430

    🔧 find_node function replaced with get_node_or_null

--- scripts/global.gd
+++ scripts/global.gd
@@ -3,10 +3,10 @@ extends Node
 
 enum BoxState {NOTOPENED, OPENED, LOCKED}
 
-onready var root: Node2D = get_tree().root.get_child(1)
-onready var player: KinematicBody2D = root.find_node('player')
-onready var chests: Node2D = root.find_node('chests')
-onready var hint: HBoxContainer = root.find_node('hint')
-onready var scroll: Control = root.find_node('scroll')
-onready var ui: Control = root.find_node('ui')
-onready var mouse: Node2D = root.find_node('mouse')
+onready var root: Node = get_tree().root.get_child(1)
+onready var player: KinematicBody2D = root.get_node_or_null('player')
+onready var chests: Node2D = root.get_node_or_null('chests')
+onready var hint: HBoxContainer = root.get_node_or_null('cameraCanvas/hint')
+onready var scroll: Control = root.get_node_or_null('cameraCanvas/scroll')
+onready var ui: Control = root.get_node_or_null('cameraCanvas/ui')
+onready var mouse: Node2D = root.get_node_or_null('cameraCanvas/mouse')
