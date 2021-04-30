9246bb691e52ff19b319f0688c8242d3fd50a4af
Mahmood Heidari <mahmoodh1378@gmail.com>
2021-04-30 15:35:37 +0430

    ✏️ login button name changed to signin,✏️ button_up function name changed to _on_signinButton_button_up

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -6,7 +6,7 @@ onready var name_label: Label = find_node('nameLabel')
 func _on_quitButton_button_up():
 	get_tree().quit(0)
 
-func _on_loginButton_button_up():
+func _on_signinButton_button_up():
 	Global.goto_scene(Global.signin_scene)
 
 func _on_startButton_button_up():
