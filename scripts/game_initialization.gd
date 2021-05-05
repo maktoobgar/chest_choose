b6d75284c684753dfe4dc22bfe3328ffe83a5d26
Mahmood Heidari <mahmoodh1378@gmail.com>
2021-05-06 00:46:05 +0430

    ✅ chest replacement function added

--- scripts/game_initialization.gd
+++ scripts/game_initialization.gd
@@ -1,6 +1,15 @@
 extends Node2D
 
 
+const Chest: Resource = preload("res://scenes/instances/chest.tscn")
+
 func _ready():
 	Global._ready()
 	Global.select_character_func()
+	replace_chest_pivots()
+
+func replace_chest_pivots():
+	for chest_place in Global.chests.get_children():
+		var chest: Area2D = Chest.instance()
+		chest.position = chest_place.position
+		chest_place.replace_by(chest, false)
